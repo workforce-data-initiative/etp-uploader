98d49ce41dc8f8ac298d30c3dcb4692990b19aef
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-06-22 22:49:18 +0300

    [#147333917] edit habitat init and run configurations

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -6,7 +6,7 @@ pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_upstream_url="https://github.com/workforce-data-initiative/etp-uploader.git"
 pkg_exports=([port]=listening_port)
 pkg_exposes=(port)
-pkg_build_deps=(core/virtualenv core/postgresql core/gcc)
+pkg_build_deps=(core/virtualenv core/gcc core/glibc)
 pkg_deps=(core/python core/coreutils)
 pkg_interpreters=(bin/python3)
 pkg_licence=('MIT')
@@ -26,15 +26,25 @@ do_unpack() {
   mkdir -p $pkg_prefix
   # copy the contents of the source directory to the habitat cache path
   build_line "Copying project data /src to $pkg_prefix ..."
-  cp -r $PROJECT_ROOT/web $pkg_prefix/
-  cp -r $PROJECT_ROOT/tests $pkg_prefix/
-  cp -r $PROJECT_ROOT/*.py $pkg_prefix/
-  cp -r $PROJECT_ROOT/requirements $pkg_prefix/
-  cp -r $PROJECT_ROOT/requirements.txt $pkg_prefix/
+  build_line "Starting with web components, config and theme ..."
+  mkdir -p $pkg_prefix/web
+  cp -vr $PROJECT_ROOT/web $pkg_prefix
+  build_line "Copying factory file ..."
+  cp -vr $PROJECT_ROOT/web/factory.py $pkg_prefix/web/factory.py
+  build_line "Copying tests ..."
+  cp -vr $PROJECT_ROOT/tests $pkg_prefix/
+  cp -vr $PROJECT_ROOT/*.py $pkg_prefix/
+  build_line "Copying requirements ..."
+  cp -vr $PROJECT_ROOT/requirements $pkg_prefix/
+  cp -vr $PROJECT_ROOT/requirements.txt $pkg_prefix/
+  build_line "Copying build file and bower.json ..."
+  cp -vr $PROJECT_ROOT/build.sh $pkg_prefix
+  cp -vr $PROJECT_ROOT/bower.json $pkg_prefix
 }
 
 do_build() {
   return 0
+  # ./build.sh
 }
 
 do_install() {
